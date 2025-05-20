5f2ab11a1f24cd9b40439b0d2bc7615195d99afa
Diego Peralta M <diego.pmz08@gmail.com>
2025-05-19 22:58:33 -0600

    Se creo la estructura base del proyecto en spring boot

---
+++ src/main/java/io/github/diegopmz/note_taking_api/NoteTakingApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.diegopmz.note_taking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/diegopmz/note_taking_api/NoteTakingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.diegopmz.note_taking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
